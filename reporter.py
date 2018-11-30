e3be29f936c9a5b12f2c1973d2c4bc56216ff0b0
Samuele Pilleri <samuele.pilleri@certimeter.it>
2018-11-30 10:13:40 +0100

    Full coverage of 5 different profiles

--- reporter.py
+++ reporter.py
@@ -4,35 +4,68 @@
 from __future__ import unicode_literals
 
 import json
+import os
 import re
 import sys
 import zipfile
 
 from codecs import getreader
-from glob import iglob
-from os.path import isfile
 
 import jsonschema
+
+from jsonref import JsonLoader, JsonRef
+
+BASE_URI = 'http://localhost:8080/'
 
 def die(msg):
 	sys.stderr.write('{0}\n'.format(msg))
 	sys.exit(-1)
 
 def header(title):
-	print('{0:{1}^80}'.format(' %s ' % title, '#'))
+	print('{0:#^80}'.format(' %s ' % title))
 
-def load_raw_unicode_escaped_latin1_encoded_json(file):
+def read(filename):
+	with open(filename, 'r') as file:
+		return file.read()
+
+def load_facebook_json(file):
 	reader = getreader('raw_unicode_escape')(file)
-	return json.loads(file.read().encode('latin1').decode('utf8', errors='replace'))
+	return json.loads(reader.read().encode('latin1').decode('utf8'), strict=False)
+
+def validate(filename, instance, schema):
+	loader = JsonLoader(store=cache)
+	schema = JsonRef.replace_refs(schema, jsonschema=True, loader=loader)
+
+	# Thank you https://github.com/pudo/jsonmapping/blob/4cf0a2/tests/util.py#L25
+	resolver = jsonschema.RefResolver(BASE_URI, BASE_URI, store=cache)
+
+	try:
+		jsonschema.validate(instance, schema, cls=jsonschema.Draft4Validator, resolver=resolver)
+	except jsonschema.SchemaError as error:
+		print('Invalid schema {0}: {1}'.format(filename, error.message))
+	except jsonschema.ValidationError as error:
+		print('Invalid instance {0}: {1}'.format(filename, error.message))
+	except Exception as error:
+		print('Exception in {0}: {1}'.format(filename, str(error)))
 
 def main(args):
-	if len(args) == 0 or not isfile(args[0]):
+	if len(args) == 0 or not zipfile.is_zipfile(args[0]):
 		die('Usage: %s <dump.zip>' % sys.argv[0])
 
 	with zipfile.ZipFile(args[0], 'r') as dump:
 
-		schemas = frozenset(iglob('*/*.json'))
-		files = frozenset(filter(lambda f: f.endswith('.json'), dump.namelist()))
+		schemas = frozenset([
+			os.path.join(dirpath, file)[2:].replace(os.path.sep, '/')
+			for dirpath, _, files in os.walk('.')
+			for file in files if file.endswith('.json')
+		])
+		files = frozenset([ file for file in dump.namelist() if file.endswith('.json') ])
+
+		#
+		# Populate shared cache for offline schema resolution
+		#
+		global cache
+		cache = { BASE_URI + file: json.loads(read(file)) for file in schemas }
 
 		print('Filename: {0}\n'.format(args[0]))
 
@@ -40,9 +73,11 @@ def main(args):
 		# Files not covered by a schema
 		#
 		header('Files not covered by a schema')
-		missings = frozenset(filter(lambda f: \
-			not f.startswith('messages/') and not f.startswith('photos_and_videos/album/'),
-			files - schemas))
+		missings = frozenset([
+			file for file in files - schemas
+			if not re.match(r'messages/.+/message\.json', file)
+			and not file.startswith('photos_and_videos/album/')
+		])
 		print('%s\n' % '\n'.join(missings) if len(missings) > 0 else '' )
 
 		#
@@ -50,50 +85,28 @@ def main(args):
 		#
 		header('Schema Conformance')
 		for file in schemas.intersection(files):
-			with dump.open(file, 'r') as i, open(file, 'r') as s:
-				schema = json.load(s)
-				instance = load_raw_unicode_escaped_latin1_encoded_json(i)
-
-			try:
-				jsonschema.validate(instance, schema, cls=jsonschema.Draft4Validator)
-			except jsonschema.SchemaError as error:
-				print('Invalid schema {0}: {1}'.format(file, error.message))
-			except jsonschema.ValidationError as error:
-				print('Invalid instance {0}: {1}'.format(file, error.message))
+			with dump.open(file, 'r') as instance:
+				validate(file, load_facebook_json(instance), cache[BASE_URI + file])
 		print('')
 
 		#
 		# Check messages against message schema
 		#
 		header('Messages Schema Conformance')
-		for file in filter(lambda f: re.match(r'messages/.+/message\.json', f), files):
-			with dump.open(file, 'r') as i, open('messages/message.json', 'r') as s:
-				schema = json.load(s)
-				instance = load_raw_unicode_escaped_latin1_encoded_json(i)
-
-			try:
-				jsonschema.validate(instance, schema, cls=jsonschema.Draft4Validator)
-			except jsonschema.SchemaError as error:
-				print('Invalid schema {0}: {1}'.format(file, error.message))
-			except jsonschema.ValidationError as error:
-				print('Invalid instance {0}: {1}'.format(file, error.message))
+		for file in filter(lambda file: re.match(r'messages/.+/message\.json', file), files):
+			with dump.open(file, 'r') as instance:
+				validate(file, load_facebook_json(instance),
+					cache[BASE_URI + 'messages/message.json'])
 		print('')
 
 		#
 		# Check photos and videos against album schema
 		#
 		header('Photos and Videos Schema Conformance')
-		for file in filter(lambda f: f.startswith('photos_and_videos/album/'), files):
-			with dump.open(file, 'r') as i, open('photos_and_videos/album/album.json', 'r') as s:
-				schema = json.load(s)
-				instance = load_raw_unicode_escaped_latin1_encoded_json(i)
-
-			try:
-				jsonschema.validate(instance, schema, cls=jsonschema.Draft4Validator)
-			except jsonschema.SchemaError as error:
-				print('Invalid schema {0}: {1}'.format(file, error.message))
-			except jsonschema.ValidationError as error:
-				print('Invalid instance {0}: {1}'.format(file, error.message))
+		for file in filter(lambda file: file.startswith('photos_and_videos/album/'), files):
+			with dump.open(file, 'r') as instance:
+				validate(file, load_facebook_json(instance),
+					cache[BASE_URI + 'photos_and_videos/album/album.json'])
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
