670ee7cbc11d94dd836500a9fb498b9661c7df90
Samuele Pilleri <samuele.pilleri@certimeter.it>
2019-08-01 15:06:50 +0200

    Removed jsonref dependency

--- reporter.py
+++ reporter.py
@@ -13,29 +13,29 @@ from codecs import getreader
 
 import jsonschema
 
-from jsonref import JsonLoader, JsonRef
+BASE_URI = 'https://samuelepilleri.github.io/facebook-schemas/'
 
-BASE_URI = 'http://localhost:8080/'
 
 def die(msg):
 	sys.stderr.write('{0}\n'.format(msg))
 	sys.exit(-1)
 
+
 def header(title):
 	print('{0:#^80}'.format(' %s ' % title))
+
 
 def read(filename):
 	with open(filename, 'r') as file:
 		return file.read()
 
+
 def load_facebook_json(file):
 	reader = getreader('raw_unicode_escape')(file)
 	return json.loads(reader.read().encode('latin1').decode('utf8'), strict=False)
 
-def validate(filename, instance, schema):
-	loader = JsonLoader(store=cache)
-	schema = JsonRef.replace_refs(schema, jsonschema=True, loader=loader)
 
+def validate(filename, instance, schema):
 	# Thank you https://github.com/pudo/jsonmapping/blob/4cf0a2/tests/util.py#L25
 	resolver = jsonschema.RefResolver(BASE_URI, BASE_URI, store=cache)
 
@@ -48,6 +48,7 @@ def validate(filename, instance, schema):
 	except Exception as error:
 		print('Exception in {0}: {1}'.format(filename, str(error)))
 
+
 def main(args):
 	if len(args) == 0 or not zipfile.is_zipfile(args[0]):
 		die('Usage: %s <dump.zip>' % sys.argv[0])
@@ -59,13 +60,13 @@ def main(args):
 			for dirpath, _, files in os.walk('.')
 			for file in files if file.endswith('.json')
 		])
-		files = frozenset([ file for file in dump.namelist() if file.endswith('.json') ])
+		files = frozenset([file for file in dump.namelist() if file.endswith('.json')])
 
 		#
 		# Populate shared cache for offline schema resolution
 		#
 		global cache
-		cache = { BASE_URI + file: json.loads(read(file)) for file in schemas }
+		cache = {BASE_URI + file: json.loads(read(file)) for file in schemas}
 
 		print('Filename: {0}\n'.format(args[0]))
 
@@ -75,10 +76,9 @@ def main(args):
 		header('Files not covered by a schema')
 		missings = frozenset([
 			file for file in files - schemas
-			if not re.match(r'messages/.+/message\.json', file)
-			and not file.startswith('photos_and_videos/album/')
+			if not re.match(r'messages/.+/message\.json', file) and not file.startswith('photos_and_videos/album/')
 		])
-		print('%s\n' % '\n'.join(missings) if len(missings) > 0 else '' )
+		print('%s\n' % '\n'.join(missings) if len(missings) > 0 else '')
 
 		#
 		# Check schemas and files against theirs schemas
@@ -95,8 +95,7 @@ def main(args):
 		header('Messages Schema Conformance')
 		for file in filter(lambda file: re.match(r'messages/.+/message\.json', file), files):
 			with dump.open(file, 'r') as instance:
-				validate(file, load_facebook_json(instance),
-					cache[BASE_URI + 'messages/message.json'])
+				validate(file, load_facebook_json(instance), cache[BASE_URI + 'messages/message.json'])
 		print('')
 
 		#
@@ -105,8 +104,8 @@ def main(args):
 		header('Photos and Videos Schema Conformance')
 		for file in filter(lambda file: file.startswith('photos_and_videos/album/'), files):
 			with dump.open(file, 'r') as instance:
-				validate(file, load_facebook_json(instance),
-					cache[BASE_URI + 'photos_and_videos/album/album.json'])
+				validate(file, load_facebook_json(instance), cache[BASE_URI + 'photos_and_videos/album/album.json'])
+
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
